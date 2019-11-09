0085b947bbfbb93b26277e518987427de29bd7b5
Rahul C H <Rahul C H@DESKTOP-BO1BFIA>
2019-11-09 18:03:55 +0530

    fifth commit

---
+++ BridgeLab/src/com/bridge/lab/PowerOfTwo.java
@@ -0,0 +1,29 @@
+package com.bridge.lab;
+
+public class PowerOfTwo {
+
+	public static void main(String[] args) {
+System.out.println("ENTER A NUMBER WITH IN 31");
+int n=0;       
+// read in one command-line argument
+
+        n = Integer.parseInt(args[0]);
+  if(n>30) {
+	  System.err.println("enter value with in 31 run again with valid input");
+  }else {
+
+        int i = 0;                // count from 0 to N
+        int POWER = 1;       // the ith power of two
+
+        // repeat until i equals n
+        while (i <= n) {
+            System.out.println(i + " " + POWER);   // print out the power of two
+            POWER = 2 * POWER;                // double to get the next one
+            i = i + 1;
+        }
+
+  }
+
+	}
+
+}
