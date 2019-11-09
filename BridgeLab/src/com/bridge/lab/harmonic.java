fee90a1337766356620921a0f3f7701c30f65d11
Rahul C H <Rahul C H@DESKTOP-BO1BFIA>
2019-11-09 18:45:48 +0530

    hormonic commit

--- BridgeLab/src/com/bridge/lab/harmonic.java
+++ BridgeLab/src/com/bridge/lab/harmonic.java
@@ -4,7 +4,7 @@ public class harmonic {
 
 	public static void main(String[] args) {
 	float sum = 0;
-	
+	int u =0;
 	int n  = Integer.parseInt(args[0]);
 		for(int i = 1 ;  i<=n ; i++) {
 			sum  = sum +(float )1/i;
