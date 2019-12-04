5dda184c6d585e8deec37da71abf6f9f2a6ab4fb
Rahul-chitturi <Rahulchitturi520@gmail.com>
2019-12-04 16:12:20 +0530

    added oops to new branch

--- BridgeLab/TestCases/BasicCoreTestCases/Mycomparator.java
+++
@@ -1,8 +0,0 @@
-package BasicCoreTestCases;
-import java.util.*;
-
-public class Mycomparator implements Comparator<Integer> {
-public int compare(Integer l , Integer r) {
-	return (l<r)?1:(l>r)?-1:0;
-}
-}
