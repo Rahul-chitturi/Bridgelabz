b9811d1cea5a7a734950ec87014f337de401a3da
Rahul-chitturi <Rahulchitturi520@gmail.com>
2019-12-05 10:03:30 +0530

    all programs Updated

---
+++ cliniquemanagement/src/com/bridgelabz/cliniquemanagement/service/CliniqueService.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.cliniquemanagement.service;
+
+
+
+public interface CliniqueService {
+
+	void doctorDetails();
+
+	void readDoctorData(String key, String value, String choice);
+
+	void readPatientData(String key, String value);
+
+}
